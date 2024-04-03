99160bbf9dc29de5c47b3bfdd47586338a5a0466
Spieler727 <spieler0727@gmail.com>
2024-04-03 04:02:20 -0400

    deep comparison

--- Assignment5.js
+++ Assignment5.js
@@ -216,3 +216,36 @@ console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
 
 // Deep Comparison
+function deepEqual(val1, val2){
+    if (val1 === val2){
+        return true;
+    }
+
+    if(val1 == null || typeof val1 != "object" || val2 == null || typeof val2 != "object"){
+        return false;
+    }
+
+    let keys1 = Object.keys(val1);
+    let keys2 = Object.keys(val2);
+
+    if (keys1.length != keys2.length){
+        return false;
+    }
+
+    for (let key of keys1) {
+        if (!keys2.includes(key) || !deepEqual(val1[key], val2[key])){
+            return false;
+        }
+    }
+    return true;
+}
+let i = null
+console.log(typeof i)
+
+let obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
